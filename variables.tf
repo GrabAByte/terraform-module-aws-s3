fc5dda88107cd067e8d0dbabd7b882cd0e5d79ec
Tom Richards <twr.devops@gmail.com>
2025-08-08 12:41:53 +0100

    first commit

--- variables.tf
+++ variables.tf
@@ -21,23 +21,23 @@ variable "bucket_version_expiry" {
   default     = 90
 }
 
-variable "kms_enable_rotation" {
-  type        = bool
-  description = "whether to expire delete markers"
-  default     = true
-}
+# variable "kms_enable_rotation" {
+#   type        = bool
+#   description = "whether to expire delete markers"
+#   default     = true
+# }
 
-variable "kms_deletion_window_days" {
-  type        = number
-  description = "number of days until deletion of KMS key"
-  default     = 20
-}
+# variable "kms_deletion_window_days" {
+#   type        = number
+#   description = "number of days until deletion of KMS key"
+#   default     = 20
+# }
 
-variable "kms_sse_algorithm" {
-  type        = string
-  description = "the algorithm to encrpyt with"
-  default     = "aws:kms"
-}
+# variable "kms_sse_algorithm" {
+#   type        = string
+#   description = "the algorithm to encrpyt with"
+#   default     = "aws:kms"
+# }
 
 variable "log_bucket_name" {
   type        = string
